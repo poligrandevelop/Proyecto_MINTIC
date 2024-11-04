1e111ea76e143d0cef34bc6c7512175cfb3a5d26
Yeyita <167666599+poligrandevelop@users.noreply.github.com>
2024-11-04 13:00:05 -0500

    Update index.js

--- ejercicios/MONGOO/Backend/src/index.js
+++ ejercicios/MONGOO/Backend/src/index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const connectionDB = require('../config/db');
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const bodyParser= require('body-parser')
+const connectionDB = require('../config/db')
+const app = express()
+const port = process.env.PORT ||3000
 
-const app = express();
-const port = 3000;
-
+require('dotenv').config()
 connectionDB();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json())
 app.use('/api/clientes', require('../routes/routesCli'));
 app.use('/api/productos', require('../routes/routesProd'));
 app.use('/api/user', require('../routes/authController'));
@@ -16,4 +17,3 @@ app.use('/api/tecnicos', require('../routes/routesTec'));
 
 app.get('/', (req, res) => res.send('Hola Programador!'));
 app.listen(port, () => console.log(`El servidor se está conectando por el puerto ${port}!`));
-
